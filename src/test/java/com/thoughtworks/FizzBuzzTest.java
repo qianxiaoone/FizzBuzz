52627c4552cf5e39857fb0837f878a785d02d29f
wanghc00 <306093226@qq.com>
2020-06-15 19:43:27 +0800

    [王宏昌]- 增加shoud_return_Fizz_when_input_30

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
     @Test
-    public void shoud_return_1_when_input_1(){
+    public void shoud_return_1_when_input_1() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -17,7 +17,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_Fizz_when_input_3(){
+    public void shoud_return_Fizz_when_input_3() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -29,7 +29,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_Buzz_when_input_5(){
+    public void shoud_return_Buzz_when_input_5() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -41,7 +41,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_FizzBuzz_when_input_15(){
+    public void shoud_return_FizzBuzz_when_input_15() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -53,7 +53,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_Whizz_when_input_7(){
+    public void shoud_return_Whizz_when_input_7() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -65,7 +65,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_BuzzWhizz_when_input_70(){
+    public void shoud_return_BuzzWhizz_when_input_70() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -77,7 +77,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void shoud_return_FizzBuzzWhizz_when_input_105(){
+    public void shoud_return_FizzBuzzWhizz_when_input_105() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -87,13 +87,26 @@ public class FizzBuzzTest {
         //then
         Assert.assertEquals("FizzBuzzWhizz", fizzBuzz.say(num));
     }
+
     @Test
-    public void shoud_return_Fizz_when_input_13(){
+    public void shoud_return_Fizz_when_input_13() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         //when
         int num = 13;
+
+        //then
+        Assert.assertEquals("Fizz", fizzBuzz.say(num));
+    }
+
+    @Test
+    public void shoud_return_Fizz_when_input_30() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        int num = 30;
 
         //then
         Assert.assertEquals("Fizz", fizzBuzz.say(num));
