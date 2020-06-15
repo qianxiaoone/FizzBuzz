22b739780c5d86385de3a7b46d8cbc5e646b1d2a
wanghc00 <306093226@qq.com>
2020-06-15 14:32:53 +0800

    导入工程

---
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void shoud_return_1_when_input_1(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+
+        //then
+    }
+}
