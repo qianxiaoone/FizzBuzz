400a5710ebecf92986909763df6c78243cabac1f
wanghc00 <306093226@qq.com>
2020-06-15 15:13:31 +0800

    [王宏昌]- 增加输入5输出Buzz的方法

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -5,6 +5,9 @@ public class FizzBuzz {
         if(num%3 == 0){
             return "Fizz";
         }
+        if(num%5 == 0){
+            return "Buzz";
+        }
         return String.valueOf(num);
     }
 }
