90b4e5baa4920bdc868b21b1d3a54dc533a97e63
306093226wang <306093226@qq.com>
2020-06-16 10:30:18 +0800

    增加MarsRover类和测试方法

---
+++ src/main/java/com.thoughtworks/MarsRover.java
@@ -0,0 +1,8 @@
+package com.thoughtworks;
+
+public class MarsRover {
+    public String execute(String initLocation, String moveCommand) {
+
+        return null;
+    }
+}
---
+++ src/test/java/com/thoughtworks/MarsRoverTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MarsRoverTest {
+    @Test
+    public void should_return_01N_when_input_00N_M(){
+        //given
+        MarsRover marsRover = new MarsRover();
+        //when
+        String location = marsRover.execute("00N", "M");
+        //then
+        Assert.assertEquals("01N",location);
+    }
+    @Test
+    public void should_return_00W_when_input_00N_L(){}
+    @Test
+    public void should_return_00E_when_input_00N_R(){}
+    @Test
+    public void should_return_0n1S_when_input_00S_M(){}
+    @Test
+    public void should_return_00E_when_input_00S_L(){}
+    @Test
+    public void should_return_00S_when_input_00S_R(){}
+    @Test
+    public void should_return_10E_when_input_00E_M(){}
+    @Test
+    public void should_return_00N_when_input_00E_N(){}
+    @Test
+    public void should_return_00S_when_input_00E_R(){}
+    @Test
+    public void should_return_n10W_when_input_00W_M(){}
+    @Test
+    public void should_return_00S_when_input_00W_L(){}
+    @Test
+    public void should_return_00N_when_input_00W_R(){}
+    @Test
+    public void should_return_31W_when_input_00N_MMMRM(){}
+}
